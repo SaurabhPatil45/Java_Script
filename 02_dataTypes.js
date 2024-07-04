03c0ed2829236644fad1b33a8c8f496b9f88f9ce
Saurabh Patil <136604351+SaurabhPatil45@users.noreply.github.com>
2024-07-04 16:49:14 +0000

    020724

---
+++ 02_dataTypes.js
@@ -0,0 +1,14 @@
+"Use strict"; // this line treat all JS code as new version
+
+//alert(3 + 3) //we are using nodejs, not browser so this give error
+
+
+//Primitive Datatype
+//number => 2 to power 53
+//string=> " "
+//boolean=> True/false
+//null => standalone value (Empty value)
+//undefined=> value is not assigned
+//symbol=>used for uniquness
+
+console.log(typeof null);//object
---
+++ 03_conversionOperation.js
@@ -0,0 +1,58 @@
+let score="33"
+
+console.log(typeof score);  //typeof is used by two ways
+console.log(typeof(score));
+
+let valueInNumber= Number(score)   // It is used to convert in umber
+console.log(typeof valueInNumber);
+
+
+//----------------------------------------------------------------------------
+
+let a="a15b16"
+
+let b=Number(a)
+console.log(b);             //NaN (Not a Number)
+
+//-------------------------------------------------------------------------
+
+let c=null
+let d=undefined
+
+let e=Number(c)
+console.log(e);//0
+let f=Number(d)
+console.log(f);//NaN
+
+
+//"33" => 33
+//"33abc" => NaN
+//true =>1 ; false =>0
+
+//----------------------------------------------------------------------------
+
+let login=1
+let booleanLogin=Boolean(login);
+console.log(booleanLogin);           //true
+
+//------------------------------------------------------------------------
+
+let str=""
+let st=Boolean(str)
+console.log(st);                 //false
+
+let str1="saur"
+let sttr=Boolean(str1)
+console.log(sttr);               //true
+
+//true =>1
+//false =>0
+//"" => false
+//"Ram" => true
+
+//------------------------------------------------------------------------------
+ let number=33
+
+ let nm=String(number)
+ console.log(nm);
+ console.log(typeof nm);
