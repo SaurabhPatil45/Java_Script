667b852290c40ae1e6f6a0a90fde97d307617ad9
Saurabh Patil <136604351+SaurabhPatil45@users.noreply.github.com>
2024-07-05 17:03:41 +0000

    050724

---
+++ 05_comparision.js
@@ -0,0 +1,13 @@
+console.log("2">1);  //true
+console.log("02">1); //true
+//String is converted to number
+
+console.log(null >0);  //false
+console.log(null==0);  //false
+console.log(null>=0);  //true 
+
+
+
+//=== : it is called strict check. It check number and Datatype also
+
+console.log(2==="2");
